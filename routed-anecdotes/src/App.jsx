bb379fff3569d9d9eb76f333a6543d28b86cf7b7
Nick Meinhold <nick@enspyr.co>
2025-11-14 00:36:56 +1100

    Completed 7.3

--- routed-anecdotes/src/App.jsx
+++ routed-anecdotes/src/App.jsx
@@ -1,10 +1,11 @@
-import { useState } from "react";
-import { Routes, Route, Link, useMatch } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { Routes, Route, Link, useMatch, useNavigate } from "react-router-dom";
 import CreateNew from "./components/CreateNew";
 import Footer from "./components/Footer";
 import About from "./components/About";
 import AnecdoteList from "./components/AnecdeoteList";
 import Anecdote from "./components/Anecdote";
+import Notification from "./components/Notification";
 
 const Menu = () => {
   const padding = {
@@ -26,6 +27,8 @@ const Menu = () => {
 };
 
 const App = () => {
+  const navigate = useNavigate();
+
   const [anecdotes, setAnecdotes] = useState([
     {
       content: "If it hurts, do it more often",
@@ -50,9 +53,21 @@ const App = () => {
     ? anecdotes.find((anecdote) => anecdote.id === Number(match.params.id))
     : null;
 
+  useEffect(() => {
+    if (notification) {
+      const timer = setTimeout(() => {
+        setNotification("");
+      }, 5000);
+
+      return () => clearTimeout(timer);
+    }
+  }, [notification]);
+
   const addNew = (anecdote) => {
     anecdote.id = Math.round(Math.random() * 10000);
     setAnecdotes(anecdotes.concat(anecdote));
+    setNotification(`Added ${anecdote.content}`);
+    navigate("/");
   };
 
   const anecdoteById = (id) => anecdotes.find((a) => a.id === id);
@@ -72,6 +87,7 @@ const App = () => {
     <>
       <Menu />
       <h1>Software anecdotes</h1>
+      <Notification message={notification} />
       <Routes>
         <Route
           path="/anecdotes/:id"
---
+++ routed-anecdotes/src/components/Notification.jsx
@@ -0,0 +1,16 @@
+const Notification = ({ message }) => {
+  if (!message) {
+    return null;
+  }
+
+  const style = {
+    border: "solid",
+    padding: 10,
+    borderWidth: 1,
+    marginBottom: 10,
+  };
+
+  return <div style={style}>{message}</div>;
+};
+
+export default Notification;
