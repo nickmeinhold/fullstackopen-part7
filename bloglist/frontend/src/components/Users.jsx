08ef6b413c4f6fcf4a9080b9703c5beaa6921090
Nick Meinhold <nick@enspyr.co>
2025-11-14 18:04:09 +1100

    Completed missed 7.14

--- bloglist/frontend/src/components/Users.jsx
+++ bloglist/frontend/src/components/Users.jsx
@@ -1,8 +1,57 @@
+import { useQuery } from "@tanstack/react-query";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Paper,
+  Typography,
+} from "@mui/material";
+import usersService from "../services/users";
+
 const Users = () => {
+  const {
+    data: users = [],
+    isLoading,
+    isError,
+  } = useQuery({
+    queryKey: ["users"],
+    queryFn: usersService.getAll,
+  });
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (isError) {
+    return <div>Error loading users</div>;
+  }
+
   return (
     <div>
-      <h2>Users</h2>
-      <p>Users list coming soon...</p>
+      <Typography variant="h4" component="h2" sx={{ marginTop: 2 }}>
+        Users
+      </Typography>
+      <TableContainer component={Paper} sx={{ marginTop: 2 }}>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>User</TableCell>
+              <TableCell>Blogs created</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {users.map((user) => (
+              <TableRow key={user.id}>
+                <TableCell>{user.name}</TableCell>
+                <TableCell>{user.blogs.length}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
     </div>
   );
 };
